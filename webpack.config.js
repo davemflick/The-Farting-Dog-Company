f4c9211cfc72e61c4b32798c75bd02f9aa178474
davemflick <davemflick@yahoo.com>
2017-05-16 14:16:54 -0400

    initial file setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: __dirname, 
+    filename: 'bundle.js',
+    publicPath: '/app/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'app'),
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
+
+//TO RUN -> webpack-dev-server --progress --colors
+
+//Help from https://blog.hellojs.org/setting-up-your-react-es6-development-environment-with-webpack-express-and-babel-e2a53994ade
