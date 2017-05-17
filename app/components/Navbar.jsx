da858e570bb4acaa03ef76f7f82c06f918a4410f
davemflick <davemflick@yahoo.com>
2017-05-17 13:19:04 -0400

    changed banner and nav styling a little big

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 export default class Navbar extends React.Component{
   render(){
     return(
+      <div>
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="navbar-header"> 
-          <a className="navbar-brand" href="#"><img className="navLogo" alt="brand" src="./app/static/images/logo1.png" /><strong className="companyName">Home</strong></a>
+          <a className="navbar-brand" href="#"><div className="navLogo" ></div></a>
           <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span className="sr-only">toggle navigation</span>
             <span className="icon-bar"></span>
@@ -19,6 +20,7 @@ export default class Navbar extends React.Component{
           <li><a className='pdLink' href="#contact">Contact</a></li>
         </ul>
       </nav>
+      </div>
     )
   }
 }
