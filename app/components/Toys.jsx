997b3b0b18aea267cbae453831e315b09b9e80ac
davemflick <davemflick@yahoo.com>
2017-05-18 09:55:55 -0400

    added the two rope toys

--- app/components/Toys.jsx
+++ app/components/Toys.jsx
@@ -7,8 +7,30 @@ const GIRAFFE = {
 	description: "Gilbert has a high fiber diet and with that is a whole lotta ruckus! Squeeze Gilberts belly and be prepared for an unruly sound.",
 	about: ['HIGH QUALITY AND DURABLE Gilbert is double stitched, interior mesh lining for extra durability, and a plush outside material.',
 	'SAFE FOR YOUR FUR BABY Gilbert is double stitched, interior mesh lining for extra durability, and a plush outside material.',
+	'This toy is not indestructible and best to monitor your pet while playing with it. If your pet finds access to the sound insert, please throw away. The sound module is not for play if exposed.',
 	],
 	more: 'Gilbert the Farting Giraffe is a great excuse to blame the dog! Made of plush, non-toxic materials, the toy is designed for extra durability and extra laughs. Made in China. Non-toxic polyfill interior and the sound insert is ROHS compliant.',
+}
+
+const ROPE_BONE ={
+	id: 'Bone_Shape',
+	name: "Bone Shape Rope Toy",
+	pic: './app/static/images/boneRope.jpeg',
+	description: "Made of 100% cotton. Tightly woven rope shaped into a bone, durable and non-toxic material ideal for your pets play",
+	about: ['HEALTHY FOR YOUR PETS TEETH AND GUMS fun way to keep up on your pets dental hygiene!',
+	' DESIGNED FOR SMALL TO MEDIUM DOGS This toy is made of high quality, woven-rope ideal for small to medium sized dogs!',
+	' SOAK IN WATER AND FREEZE Great summer treat when it’s a little toasty outside!',],
+}
+
+const TUG_N_TOSS = {
+	id: 'Tug_N_Toss',
+	name: "Tug-n-Toss Rope Toy",
+	pic: './app/static/images/tugNToss.jpeg',
+	description: "Made of 100% cotton. Tightly woven rope, durable and non-toxic material ideal for your pets play",
+	about: [' GREAT TOY TO INTERACT WITH YOUR PET Handle makes it easy to interact with your pup, play tug-of-war, and toss!',
+	'HEALTHY FOR YOUR PETS TEETH AND GUMS fun way to keep up on your pets dental hygiene!',
+	' DESIGNED FOR SMALL TO MEDIUM DOGS This toy is made of high quality, woven-rope ideal for small to medium sized dogs!',
+	' SOAK IN WATER AND FREEZE Great summer treat when it’s a little toasty outside!',],
 }
 
 const PROCEEDS = { strong: "5% OF PROFITS DONATED FOR HOMELESS PETS",
@@ -18,7 +40,7 @@ export default class Toys extends React.Component{
 	
 	createToy(toy){
 		return(
-			<div className="individualToys">
+			<div className="individualToys" id={toy.id}>
 			  <div className="toyImage">
 			   <img className="imageOfToy" src={toy.pic} alt={toy.name} />
 			  </div>
@@ -43,6 +65,8 @@ export default class Toys extends React.Component{
 			 <h4 className="fivePercent"><strong>{PROCEEDS.strong} </strong> {PROCEEDS.descrip}</h4>
 			</div>
 				{this.createToy(GIRAFFE)}
+				{this.createToy(ROPE_BONE)}
+				{this.createToy(TUG_N_TOSS)}
 			</div>
 		)
 	}
